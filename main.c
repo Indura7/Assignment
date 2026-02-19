26d3339314fb70de6927d8471f48a3975e0dbc46
Indura7 <160689278+Indura7@users.noreply.github.com>
2026-02-19 22:43:36 +0530

    Add CodeBlocks layout and menu-driven main
    Add a Code::Blocks project layout and a new text main file; refactor main.c to present a simple menu (Compare Sets / Display Pipes / Exit), increase set size, and wire up function calls. Clean up source.c formatting and comments, rename some local variables (v -> f) and adjust output formatting for comparisons and pipe display. Update dependency timestamps, add/replace various build artifacts (Debug/Release object and exe files), and remove source.exe. These changes enable an interactive menu for the assignment and improve readability of the helper functions.

--- main.c
+++ main.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 #include "functions.h"
-
-//Prototypes of functions
-void compare_sets(int set1[], int set2[], int size);
-void display_pipes(int pnumbers[], int psize);
 
 int main(){
 
- int size =5;           //number of elements for the two sets
- int set1[size];
- int set2[size];
+ int size =8;                                 //Size of integer sets
+ int set1[size];                              //First integer set
+ int set2[size];                              //Second integer set
 
- int psize =5;          //number of elements for pipe display
- int pnumbers[psize];
+ int psize =5;                                //Size of pipe numbers
+ int pnumbers[psize];                         //Array to hold pipe numbers
+ int x;                                       //Control function menue
 
-    compare_sets(set1, set2, size);     //Task 1
-    display_pipes(pnumbers, psize);     //Task 2
+ printf("------------------------\n   ASSIGNMENT MENU   \n------------------------\n");
+ printf(" 1. Compare Sets\n 2. Display Pipes\n 3. Exit\n------------------------\n");
+ printf("  Press whatever you like  \n");
 
+ scanf("   %d",&x);
+
+ switch(x){
+    case 1:                                           //Compare two integer sets
+        compare_sets(set1, set2, size);
+        break;
+    case 2:                                           //Display numbers with pipes
+        display_pipes(pnumbers, psize);
+        break;
+    case 3:
+        printf("Exiting...");                         //Exit message
+        break;
+    default:
+        printf("\n  Wrong input..\nPlease try again  ");
+
+    }
     return 0;
-
-
 }
+
